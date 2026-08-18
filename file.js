05d43308a25a3da17ea9b0fca5ee791c0a7de178
DHANISHAPRAJAPATI <dhanisha863@gmail.com>
2026-08-18 22:02:18 +0530

    added

---
+++ file.js
@@ -0,0 +1,5 @@
+let str = "Dhanisha";
+console.log("TOUPPARCASE:", str.toUpperCase());
+let spacestring = " Hello world ";
+console.log("trim():",spacestring.trim());
+console.log("add string:",str.includes("Prajapati"));
